e816b927b1f319fec9154adb39e25b7612f21f2a
Alex <alex@MacMini1.local>
2015-06-09 11:38:04 -0400

    Beginning Arrow Functionality
    Begun work on allowing the arrows to control which images show up in the slideshow.
    The first one will be on the left arrow

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,16 +3,21 @@ $(document).ready(function(){
 	//set the current image source to the image in the first href
 	$('img').attr('src',$('#option1').prop('href')); //used jQuery prop method o get the tagname of the first option	
 
+/*
 	//if the left selector is clicked
-		//access the closest sibling to the currently selected anchor	
-	
+		$('.leftSelect').on('click',function(){
+			//make the image source assigned to the image of the nearest sibling of the current anchor
+			$('img').attr('src', function(){
+				 $('.optionList li a').attr('href').next();
+			});
+		}); 	
+*/	
 	//When a list option has been clicked
 	$('.optionList li a').click(function(e){
 		e.preventDefault(); //prevents default action of opening a new window
 		var $anchorHref= $(this).attr('href');
 		
 		//set that options' href attribute to to source of the image
-
 		$('img').attr('src', $anchorHref); 
 			
 	}); //end of option1 click function
