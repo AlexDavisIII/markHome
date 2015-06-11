b9aac1b0463ab81de8c726d3121bee17a6ea1836
Alex <alex@MacMini1.local>
2015-06-11 11:05:54 -0400

    Changed comments and added variables for readability

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,12 +1,18 @@
 //When the document is fully loaded and ready to go
 $(document).ready(function(){
-		var x = 0; //used for rightSelect increments.
-
+		var LEFT_RESET= 4; //used to reset the right arrow 
+		var RIGHT_RESET= -1; //used to reset the left arrow
+		var x = 0; //used for incremental purposes 
+		
 		//set the current image source to the image in the first href
 		$('img').attr('src',$('#option1').prop('href')); //used jQuery prop method o get the tagname of the first option	
 
 		//When the right arrow is clicked
 		$('.rightSelect').on('click',function(){
+
+			if(x == 3){
+			x = RIGHT_RESET;
+			}
 
 			x++;
 			//so long as you have not reached the max number of option spots
@@ -14,14 +20,8 @@ $(document).ready(function(){
 			//select the next sibling to the currently selected option
 				var $options = $('.optionList li a');
 				var $optionHref= $options[x].getAttribute("href");
-
-				console.log($optionHref);
-				console.log(x);
-
 				$('img').attr('src', $optionHref);
-			} else {
-				x = -1;
-			}			
+			}		
 			});
 
 
@@ -29,22 +29,17 @@ $(document).ready(function(){
 
 		$('.leftSelect').on('click',function(){
 			//so long as you have not reached the max number of option spots
-
+			if(x == 0){
+				x = LEFT_RESET; 	
+			}
 			x--;
-			if(x >= 0 && x < 4){
+			if(x >= 0 && x <= 3){
 			//select the next sibling to the currently selected option
 				var $options = $('.optionList li a');
 				var $optionHref= $options[x].getAttribute("href");
-			
-				
-				console.log($optionHref);
-				console.log(x);
-			
 
 				$('img').attr('src', $optionHref);
-			} else {
-			x = 4;
-			}			
+			} 
 			});
 
 
