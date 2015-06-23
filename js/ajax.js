f49359437ca7755de9678f0d2bde77fe0d3e0185
Alex <alex@MacMini1.local>
2015-06-23 11:50:59 -0400

    Includes the Description
    The last commit failed to include the description.
    This one fixes that by including the description to be shown!

--- js/ajax.js
+++ js/ajax.js
@@ -2,12 +2,14 @@ $(document).ready(function(){
 	var url1 = 'descriptions/description.html';
 
 	$('#textContent').load(url1, function(){
-		$(this).hide();
-		$('#textContent #description').hide();
+		$(this).hide();//hides the content container
+		$('#textContent #description').hide(); //hides the description in the container
+
 		console.log("this finished");
 		$(this).show('slow', function(){
 			$(this).ready(function(){
 				$('#textContent #description').slideDown('slow');
+				console.log('show have executed');
 			}); //end of ready function
 		}); //end of show function
 	});//end of the firstDescription load function
