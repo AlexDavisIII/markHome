774b1d786190a1986148bfc7b7e5103a93c645ca
Alex <alex@MacMini1.local>
2015-06-24 10:11:19 -0400

    jQuery Only (For The Moment)
    For browser compatibility reasons, AJAX is currently removed from the project
    The way with which AJAX was incorporated into the project led to browser compatibility issues (worked in FireFox, not in Chrome).

--- js/ajax.js
+++ js/ajax.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var url1 = 'descriptions/description.html';
+	/*var url1 = 'descriptions/description.html';
 
 	$('#textContent').load(url1, function(){
 		$(this).hide();//hides the content container
@@ -12,5 +12,16 @@ $(document).ready(function(){
 				console.log('show have executed');
 			}); //end of ready function
 		}); //end of show function
-	});//end of the firstDescription load function
+	});//end of the firstDescription load function*/
+
+//List of hidden elements when page loads
+	$('#textContent').hide();//hides the entire element
+	$('#textContent #description').hide();//hides the description in the text content	
+
+
+	//after about 3 seconds, show the hidded textContent element
+	$('#textContent').show('slow', function(){
+		//show the paragraph description when ready
+		$('#description').show('slow');
+	});//end of show function
 });//when the document is ready to go
